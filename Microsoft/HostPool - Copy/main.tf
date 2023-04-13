6e9b2a2af917108304538e328c570568f44bbfc3
usarif <usarif@outlook.com>
2023-04-13 16:17:05 -0500

    test

---
+++ Microsoft/HostPool - Copy/main.tf
@@ -0,0 +1,222 @@
+# #Resource group name is output when execution plan is applied.
+resource "azurerm_resource_group" "sh" {
+  name     = var.rg_name
+  location = var.resource_group_location
+}
+
+# Create AVD workspace
+resource "azurerm_virtual_desktop_workspace" "workspace" {
+  name                = var.workspace
+  resource_group_name = azurerm_resource_group.sh.name
+  location            = azurerm_resource_group.sh.location
+  friendly_name       = "${var.prefix} Workspace"
+  description         = "${var.prefix} Workspace"
+}
+
+# Create AVD host pool
+resource "azurerm_virtual_desktop_host_pool" "hostpool" {
+  resource_group_name      = azurerm_resource_group.sh.name
+  location                 = azurerm_resource_group.sh.location
+  name                     = var.hostpool
+  friendly_name            = var.hostpool
+  validate_environment     = true
+  custom_rdp_properties    = "audiocapturemode:i:1;audiomode:i:0;targetisaadjoined:i:1;"
+  description              = "${var.prefix} Terraform HostPool"
+  type                     = "Pooled"
+  maximum_sessions_allowed = 16
+  load_balancer_type       = "BreadthFirst" #[BreadthFirst DepthFirst]
+}
+
+resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
+  hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
+  expiration_date = var.rfc3339
+}
+
+# Create AVD DAG
+resource "azurerm_virtual_desktop_application_group" "dag" {
+  resource_group_name = azurerm_resource_group.sh.name
+  host_pool_id        = azurerm_virtual_desktop_host_pool.hostpool.id
+  location            = azurerm_resource_group.sh.location
+  type                = "Desktop"
+  name                = "${var.prefix}-dag"
+  friendly_name       = "Desktop AppGroup"
+  description         = "AVD application group"
+  depends_on          = [azurerm_virtual_desktop_host_pool.hostpool, azurerm_virtual_desktop_workspace.workspace]
+}
+
+# Associate Workspace and DAG
+resource "azurerm_virtual_desktop_workspace_application_group_association" "ws-dag" {
+  application_group_id = azurerm_virtual_desktop_application_group.dag.id
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+}
+
+resource "azuread_group" "avdgroup" {
+  display_name = var.aadgroup
+  #owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
+
+#Configure Azure Virtual Desktop role-based access control using Terraform
+# resource "azuread_group_member" "aad_group_member" {      
+#   for_each         = data.azuread_user.aad_user
+#   group_object_id  = azuread_group.avdgroup.id
+#   member_object_id = each.value["id"]
+# }
+
+resource "azurerm_role_assignment" "role" {
+  scope                = azurerm_virtual_desktop_application_group.dag.id
+  role_definition_name = var.roleid
+  principal_id         = azuread_group.avdgroup.id
+}
+
+resource "random_string" "AVD_local_password" {
+  count            = var.rdsh_count
+  length           = 16
+  special          = true
+  min_special      = 2
+  override_special = "*BHP@dminJBL!!"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.sh.location
+  resource_group_name = azurerm_resource_group.sh.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.sh.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "avd_vm_nic" {
+  count               = var.rdsh_count
+  name                = "${var.prefix}-${count.index + 1}-nic"
+  resource_group_name = azurerm_resource_group.sh.name
+  location            = azurerm_resource_group.sh.location
+
+  ip_configuration {
+    name                          = "nic${count.index + 1}_config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+  }
+
+  depends_on = [
+    azurerm_resource_group.sh
+  ]
+}
+
+resource "azurerm_windows_virtual_machine" "avd_vm" {
+  count                 = var.rdsh_count
+  name                  = "${var.prefix}-${count.index + 1}"
+  resource_group_name   = azurerm_resource_group.sh.name
+  location              = azurerm_resource_group.sh.location
+  size                  = var.vm_size
+  network_interface_ids = ["${azurerm_network_interface.avd_vm_nic.*.id[count.index]}"]
+  provision_vm_agent    = true
+  admin_username        = var.local_admin_username
+  admin_password        = var.local_admin_password
+
+  os_disk {
+    name                 = "${lower(var.prefix)}-${count.index + 1}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "office-365"
+    sku       = "win10-22h2-avd-m365-g2"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_resource_group.sh,
+    azurerm_network_interface.avd_vm_nic
+  ]
+}
+
+# 
+
+
+
+resource "azurerm_virtual_machine_extension" "AADLoginForWindows" {
+  count                      = var.rdsh_count
+  name                       = "AADLoginForWindows"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher                  = "Microsoft.Azure.ActiveDirectory"
+  type                       = "AADLoginForWindows"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+  # depends_on = [
+  #     azurerm_virtual_machine_extension.dsc
+  # ]
+}
+
+# createing Network Security Group NSG
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.sh.location
+  resource_group_name = azurerm_resource_group.sh.name
+
+  security_rule {
+    name                       = "out-shortPath-udp"
+    priority                   = 2000
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "UDP"
+    source_port_range          = "*"
+    destination_port_range     = "3390"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "In-shortPath-udp"
+    priority                   = 2000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "UDP"
+    source_port_range          = "*"
+    destination_port_range     = "3390"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vmext_dsc" {
+  count                      = var.rdsh_count
+  name                       = "${var.prefix}${count.index + 1}-avd_dsc"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
+  publisher                  = "Microsoft.Powershell"
+  type                       = "DSC"
+  type_handler_version       = "2.73"
+  auto_upgrade_minor_version = true
+
+  settings = <<-SETTINGS
+    {
+      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_09-08-2022.zip",
+      "configurationFunction": "Configuration.ps1\\AddSessionHost",
+      "properties": {
+        "HostPoolName":"${azurerm_virtual_desktop_host_pool.hostpool.name}"
+      }
+    }
+SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+  {
+    "properties": {
+         "registrationInfoToken": "${local.registration_token}"
+    }
+  }
+ 
+PROTECTED_SETTINGS
+
+  depends_on = [
+    azurerm_virtual_machine_extension.AADLoginForWindows,
+    azurerm_virtual_desktop_host_pool.hostpool,
+    #data.azurerm_log_analytics_workspace.lawksp
+  ]
+}
---
+++ Microsoft/HostPool - Copy/variables.tf
@@ -0,0 +1,109 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "rg_name" {
+  type        = string
+  default     = "rg-avd-resources"
+  description = "Name of the Resource group in which to deploy service objects"
+}
+
+variable "workspace" {
+  type        = string
+  description = "Name of the Azure Virtual Desktop workspace"
+  default     = "AVD TF Workspace"
+}
+
+variable "hostpool" {
+  type        = string
+  description = "Name of the Azure Virtual Desktop host pool"
+  default     = "AVD-TF-HP"
+}
+
+variable "rfc3339" {
+  type        = string
+  default     = "2023-04-15T12:43:13Z"
+  description = "Registration token expiration"
+}
+
+variable "prefix" {
+  type        = string
+  default     = "avdtf"
+  description = "Prefix of the name of the AVD machine(s)"
+}
+
+variable "aadgroup" {
+  type        = string
+  default     = "avd_testgroup"
+  description = "Prefix of the name of the AVD machine(s)"
+}
+
+variable "roleid" {
+  type        = string
+  default     = "Desktop Virtualization User"
+  description = "Prefix of the name of the AVD machine(s)"
+}
+
+variable "rdsh_count" {
+  description = "Number of AVD machines to deploy"
+  default     = 2
+}
+
+variable "vm_size" {
+  description = "Size of the machine to deploy"
+  #default     = "Standard_DS2_v2"
+  default     = "Standard_B1s"
+}
+
+variable "local_admin_username" {
+  type        = string
+  default     = "aadjoiner"
+  description = "local admin username"
+}
+
+variable "local_admin_password" {
+  type        = string
+  default     = "Xs_!*jkljoin2"
+  description = "local admin password"
+  sensitive   = true
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "usarif.onmicrosoft.com"
+  description = "Name of the domain to join"
+}
+
+variable "domain_user_upn" {
+  type        = string
+  default     = "usarif@usarif.onmicrosoft.com" # do not include domain name as this is appended
+  description = "Username for domain join (do not include domain name as this is appended)"
+}
+
+variable "domain_password" {
+  type        = string
+  default     = "ChangeMe456!"
+  description = "Password of the user to authenticate with the domain"
+  sensitive   = true
+}
+
+variable "ou_path" {
+  default = ""
+}
+
+#Configure Azure Virtual Desktop role-based access control using Terraform
+variable "avd_users" {
+  description = "AVD users"
+  default = [
+    "avduser01@usarif.onmicrosoft.com",
+    "avduser02@usarif.onmicrosoft.com"
+  ]
+}
+
+#Configure Azure Virtual Desktop role-based access control using Terraform
+variable "aad_group_name" {
+  type        = string
+  default     = "AVDUsers"
+  description = "Azure Active Directory Group for AVD users"
+}
