bcad826020a4982646f711e3b68b74d77a248ffe
usarif <usarif@outlook.com>
2023-07-19 13:21:52 -0500

    Destroy 3

--- Microsoft/HostPool/variables.tf
+++ Microsoft/HostPool/variables.tf
@@ -3,7 +3,7 @@
 #   description = "Location of the resource group."
 # }
 
-# variable "rg_name" {
+## variable "rg_name" {
 #   type        = string
 #   default     = "rg-avd-resources"
 #   description = "Name of the Resource group in which to deploy service objects"
