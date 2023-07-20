2903a9d127072a36558f0744b06d5ca6c15a0f77
usarif <usarif@outlook.com>
2023-07-20 06:34:26 -0500

    Destroy 4

--- Microsoft/HostPool/data.tf
+++ Microsoft/HostPool/data.tf
@@ -1,9 +1,9 @@
 
-data "azuread_user" "aad_user" {
-  for_each            = toset(var.avd_users)
-  user_principal_name = format("%s", each.key)
-}
-
-# data "azurerm_role_definition" "role" { # access an existing built-in role
-#   name = "avd_testgroup"
+# data "azuread_user" "aad_user" {
+#   for_each            = toset(var.avd_users)
+#   user_principal_name = format("%s", each.key)
 # }
+
+# # data "azurerm_role_definition" "role" { # access an existing built-in role
+# #   name = "avd_testgroup"
+# # }
--- Microsoft/HostPool/providers.tf
+++ Microsoft/HostPool/providers.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>2.0"
+#     }
+#     azuread = {
+#       source = "hashicorp/azuread"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
