7ee7f9ebbe3a80949183dcd33be1f01ba0628fe8
usarif <usarif@outlook.com>
2023-07-20 06:51:42 -0500

    Build HP 10

--- Microsoft/HostPool/data.tf
+++ Microsoft/HostPool/data.tf
@@ -1,9 +1,9 @@
 
-# data "azuread_user" "aad_user" {
-#   for_each            = toset(var.avd_users)
-#   user_principal_name = format("%s", each.key)
-# }
+data "azuread_user" "aad_user" {
+  for_each            = toset(var.avd_users)
+  user_principal_name = format("%s", each.key)
+}
 
-# # data "azurerm_role_definition" "role" { # access an existing built-in role
-# #   name = "avd_testgroup"
-# # }
+# data "azurerm_role_definition" "role" { # access an existing built-in role
+#   name = "avd_testgroup"
+# }
